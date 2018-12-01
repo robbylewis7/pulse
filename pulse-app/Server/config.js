4d8d919a0fa4a8d5d7e75a73f90175366bed14c8
Robby Lewis <robbyklew@gmail.com>
2018-12-01 10:00:34 -0800

    update

---
+++ pulse-app/Server/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1/pulse';
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || 'mongodb://127.0.0.1/test-pulse-app';
+  exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = 'rosebud';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
