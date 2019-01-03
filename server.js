773a446b1e9622e14fd4235aa96b7e6b1509d47c
Robby Lewis <robbyklew@gmail.com>
2019-01-03 08:46:02 -0800

    updated server

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 //////////////////
 
 
-app.use(express.static(path.resulve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'Client')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'Client', 'index.html'))
 });
 
 
